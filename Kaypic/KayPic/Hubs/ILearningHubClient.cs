9f0a671a92d163b040a046cfa570b79aab2a3349
ebackiny <2291924@crosemont.qc.ca>
2025-12-14 16:18:12 -0500

    enlevé LearningHub

--- Kaypic/KayPic/Hubs/ILearningHubClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KayPic.Hubs
-{
-    public interface ILearningHubClient
-    {
-        Task ReceiveMessage(string message);
-        Task Notify(string notification);
-        Task ReceiveGroupUsers(string groupName, List<string> users);
-    }
-}
